8af5b51894268a7f8c6f951d5ed2c716735d03ac
kvalp <kvale010@contratistas.codelco.cl>
2023-09-27 04:38:12 -0300

    intento de cambio de db vectorial

--- bd_connection.py
+++ bd_connection.py
@@ -1,47 +1,38 @@
-from langchain.vectorstores import Chroma
-from langchain.embeddings import TensorflowHubEmbeddings
+import lancedb
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-import chromadb
-from chromadb.config import Settings
+embeddings = OpenAIEmbeddings()
+uri = "data/sample-lancedb"
+db = lancedb.connect(uri)
 
-setting = Settings(
-    chroma_api_impl = "rest",
-    chroma_server_host = "localhost",
-    chroma_server_http_port = 8000
-)
+def create_table(table_name, data):
+    table = db.create_table(name=table_name, data=data)
+    return table
 
-client = chromadb.Client(setting)
-
-client.get_version()
-
-def create_collection():
-    client.create_collection(
-        name = "cvs",
-        embedding_function = OpenAIEmbeddings(),
-        get_or_create = True
-    )
+def query_db(query, documents, table):
+    docsearch = db.from_documents(documents, embeddings, connection=table)
+    return docsearch
 
 
-def delete_collection(name_collection):
-    client.delete_collection(name = name_collection)
+# def delete_collection(name_collection):
+#     client.delete_collection(name = name_collection)
 
 
-def add_new_document(name_collection, ids, document):
-    collection = client.get_collection(name = name_collection, embedding_function= OpenAIEmbeddings())
-    collection.add(
-        ids = ids, 
-        documents = document
-    )
+# def add_new_document(name_collection, ids, document):
+#     collection = client.get_collection(name = name_collection, embedding_function= OpenAIEmbeddings())
+#     collection.add(
+#         ids = ids, 
+#         documents = document
+#     )
 
-def get_cvs(name_collection, query_text):
-    collection = client.get_collection(name = name_collection, embedding_function= OpenAIEmbeddings())
-    results = collection.query(
-        query_texts=query_text,
-        n_results=5,
-        #where_document = {'$contains': 'skill'} # aqui se podria filtrar por skills
-    )
-    return results
+# def get_cvs(name_collection, query_text):
+#     collection = client.get_collection(name = name_collection, embedding_function= OpenAIEmbeddings())
+#     results = collection.query(
+#         query_texts=query_text,
+#         n_results=5,
+#         #where_document = {'$contains': 'skill'} # aqui se podria filtrar por skills
+#     )
+#     return results
     
 
 
--- handle_pdfs.py
+++ handle_pdfs.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfReader
-from bd_connection import save_docs_db
+from bd_connection import create_table
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 
@@ -8,15 +8,15 @@ def read_pdf(pdf):
     loader = PyPDFLoader(pdf)
     pages = loader.load_and_split()
     
-    #pdf_reader = PdfReader(pdf)
-    # text = ""
-    # for page in pdf_reader.pages:
-    #     text += page.extract_text()
-    # make_chunks(text)
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    make_chunks(text)
 
 
 def make_chunks(text):
     text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200, length_function=len)
     chunks = text_splitter.split_text(text)
-    save_docs_db(chunks)
+    create_table(chunks)
     
